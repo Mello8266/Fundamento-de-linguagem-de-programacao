f9d33617e87445fe11691fbfefc7c055cf8411ae
Mello8226 <vinnycius.melo@gmail.com>
2026-03-09 21:01:15 -0300

    Segunda aula - teórico

---
+++ segundaAula/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace MeuProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int x = 5;
+            // int y = 10;
+
+            // Console.WriteLine(x);
+
+            // int a = 3;
+            // int b = 5;
+            // int c = a * b;
+
+            // int a;
+
+            // a = 3;
+            // a = 7;
+
+            // a = a + 3;
+
+            // Console.WriteLine(a);
+
+
+            // Exercício 1
+            // int A, B, C, X;
+            // A = 1;
+            // B = 5;
+            // C = A * B;
+            // A = A + C;
+            // X = B * C;
+            // X = X + 3;
+            // A = X - B;
+            // X = X * X;
+            // Console.WriteLine(X);
+
+            // Exercício 2
+            // int Z, B, C, D;
+            // Z = 3;
+            // B = Z * 2;
+            // C = 4;
+            // D = 3;
+            // Z = B + C + D;
+            // Z = Z * Z;
+            // Z = C - Z;
+
+            // Console.WriteLine(Z);
+
+            // Exercício 3
+            // int L, D, H, J, M, F;
+            // L = 5;
+            // D = 45;
+            // H = D / L;
+            // H = H + L + D;
+            // J = 5;
+            // M = 6;
+            // F = D;
+            // H = F;
+
+            // Console.WriteLine(H);
+
+            // Exercício 4
+            int AD, XJ, M, DD;
+            AD = 45;
+            XJ = 21;
+            M = AD + XJ;
+            DD = 3 * M + XJ;
+            XJ = 12;
+            DD = XJ * M - DD;
+            M = 5 * M;
+
+            Console.WriteLine(M);
+        }
+    }
+}
