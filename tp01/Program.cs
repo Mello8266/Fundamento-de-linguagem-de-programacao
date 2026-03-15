aebb64585bb6050e1668bf567a4fb604cc6ca3d7
Mello8226 <vinnycius.melo@gmail.com>
2026-03-15 17:20:37 -0300

    Primeiro exercício

---
+++ tp01/Program.cs
@@ -0,0 +1,16 @@
+﻿//? Exercício 1
+// double area, b, altura;
+
+// Console.Clear();
+
+// Console.Write("Digite a base do retângulo: ");
+// b = double.Parse(Console.ReadLine());
+
+// Console.Write("Digite a altura do retângulo: ");
+// altura = double.Parse(Console.ReadLine());
+
+// area = b * altura;
+
+// Console.WriteLine($"A área do retângulo é {area}");
+
+//TODO Exercício 2
